05bf4cd4a4eb386686686a338b545e0b0f9699d6
franaragon13 <franaragon196913@gmail.com>
2021-05-05 13:48:19 +0200

    Añade tiempo de duracion de video

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React, { Component, useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import LandingPage from './Components/LandingPage/LandingPage'
-import Home from './Components/Home/Home'
 
 const App = () => {
-  const [video, setVideo] = useState(true)
-
-  const showMap = () => {
-    setTimeout(() => {
-      setVideo(false)
-    }, 5000);
-  }
-  showMap()
-
-  if (video == true) {
-    return (
-      <Router>
-        <Switch>
-          <Route path='/' exact component={LandingPage} />
-        </Switch>
-      </Router>
-    )
-  } else {
-    return (
-      <Router>
-        <Switch>
-          <Route path='/' exact component={Home} />
-        </Switch>
-      </Router>
-    )
-  }
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={LandingPage} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,15 +2,33 @@ import React, { Component, useState, useEffect } from 'react'
 
 //Componente que renderizará el video de la primera carga
 const LandingPage = (props) => {
-    return (
-        <div>
-            <video muted preload='auto' className='video' autoPlay playsInline>
-                <source src="../images/intro.mp4" type="video/mp4" />
-                <source src="../images/intro11.webm" type="video/webm" />
-                <img src="imagen.png" alt="Video no soportado" />
-            </video>
-        </div>
-    )
+    const [video, setVideo] = useState(true)
+
+    const showMap = () => {
+        setTimeout(() => {
+            setVideo(false)
+        }, 5000);
+    }
+
+    if (video) {
+        showMap()
+        return (
+            <div>
+                <video muted preload='auto' className='video' autoPlay playsInline>
+                    <source src="../images/intro.mp4" type="video/mp4" />
+                    <source src="../images/intro11.webm" type="video/webm" />
+                    <img src="imagen.png" alt="Video no soportado" />
+                </video>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                PAGINA
+            </div>
+        )
+    }
+
 }
 
 export default LandingPage
