96e42355fb566f607e96f87e2b0312caf8f32e1c
franaragon13 <franaragon196913@gmail.com>
2021-05-12 16:54:00 +0200

    Añade funcionalidad a apartado mis pedidos

--- src/Components/User/MisPedidos.js
+++ src/Components/User/MisPedidos.js
@@ -1,4 +1,5 @@
-import React, { Component, useState, useEffect, Fragment } from 'react'
+import React, { Component, useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { Form } from 'reactstrap'
 import Cookies from 'universal-cookie'
 import { Link } from 'react-router-dom'
@@ -7,23 +8,62 @@ import swal from 'sweetalert';
 
 //Componente que renderizará el video de la primera carga
 const MisPedidos = (props) => {
+    const { id } = useParams()
+    const cookies = new Cookies()
+    const [listaPedidos, setListaPedidos] = useState([]);
 
-    return (
-        <div className='login_container'>
-            <Link to='/home'>
-                <picture>
-                    <img alt='logotipo' id='logotipo_login' src='../images/logotipo_en_color.png'></img>
-                </picture>
-            </Link>
-            <div className='datosPersonales'>
-                <h2>MIS PEDIDOS</h2>
-                <h4 className='mensajeMisPedidos'>Actualmente no existe ningún pedido.</h4>
-                <Link className='linkDatosPersonales' to='/login'>
+    useEffect(() => {
+        Axios.get(`https://artinkoo.herokuapp.com/misPedidos/${cookies.get('idUsuario')}`).then((response) => {
+            setListaPedidos(response.data)
+            console.log(response);
+        })
+    }, [])
+    console.log(listaPedidos);
+    if (listaPedidos.length > 0) {
+        return (
+            <div className='login_container'>
+                <Link to='/home'>
+                    <picture>
+                        <img alt='logotipo' id='logotipo_login' src='../images/logotipo_en_color.png'></img>
+                    </picture>
+                </Link>
+                <h2 className='titulo'>MIS PEDIDOS</h2>
+                <div className='misPedidosContainer'>
+                    {
+                        listaPedidos.map((pedido, index) => {
+                            return (
+                                <div className='misPedidos'>
+                                    <p><span className='columnaDatosPersonales'>Identificador Pedido: </span>{pedido.idPedido}.</p>
+                                    <p><span className='columnaDatosPersonales'>Productos: </span>{pedido.productosPedido}.</p>
+                                    <p><span className='columnaDatosPersonales'>Importe: </span>{pedido.importe}€.</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <Link className='linkMisPedidos' to='/login'>
                     Volver al Panel de Usuario
-                    </Link>
+                        </Link>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className='login_container'>
+                <Link to='/home'>
+                    <picture>
+                        <img alt='logotipo' id='logotipo_login' src='../images/logotipo_en_color.png'></img>
+                    </picture>
+                </Link>
+                <div className='datosPersonales'>
+                    <h2>MIS PEDIDOS</h2>
+                    <h4 className='mensajeMisPedidos'>Actualmente no cuentas con ningún pedido en el historial.</h4>
+                    <Link className='linkDatosPersonales' to='/login'>
+                        Volver al Panel de Usuario
+                        </Link>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default MisPedidos
