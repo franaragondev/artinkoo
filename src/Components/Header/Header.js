c986d40720163c76f6a6726192bdac18909b78ed
franaragon13 <franaragon196913@gmail.com>
2021-05-20 17:28:23 +0200

    Añade jquery y media query para cabecera y menú desplegable

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,69 +4,73 @@ import { Link } from 'react-router-dom'
 //Componente que renderizará el video de la primera carga
 const Header = (props) => {
     return (
-        <header>
-            <div id="menu">
-                <input id="hamburguer-icon" type="checkbox" />
-                <label for="hamburguer-icon">
-                    <span className="hamburguer-line"></span>
-                    <span className="hamburguer-line"></span>
-                    <span className="hamburguer-line"></span>
-                </label>
-                <ul id="menuOculto">
-                    <li>
-                        {/* <img alt="lupa" src="../images/search_magnifying_glass_icon_149392.svg" /> */}
-                        <input type="text" id="buscar" name="buscar" placeholder="Encuentra lo que buscas" />
-                        <hr />
-                    </li>
-                    <li> <a href="pages/productos.html">Productos</a></li>
-                    <li> <a href="pages/personalizacion.html">Personalización</a></li>
-                    <li> <a href="pages/aloju.html">ALOJÚ</a></li>
-                    <li> <a href="pages/contacto.html">Contáctanos</a></li>
-                </ul>
-            </div>
-
-            <div id="logoCabecera">
-                <Link to='/home'>
-                    <img alt="logoColor" src="../images/logotipo_en_color.png" />
-                </Link>
-            </div>
-
-            <div id='menu_query'>
-                <p id='productos_menu_query'><a href="#">Productos</a></p>
-                <p id='personalizacion_menu_query'><a href="#">Personalización</a></p>
-                <p id='aloju_menu_query'><a href="#">ALOJÚ</a></p>
-                <p id='contactanos_menu_query'><a href="#">Contáctanos</a></p>
-            </div>
-
-            <div id="derecha">
-                <Link to='/login'>
-                    <img id='icon_user' alt="icono_user" src="../images/icon_user.svg" />
-                </Link>
-                <input id="idioma" type="checkbox" />
-                <label for="idioma">
-                    <img alt="globoIdioma" src="../images/-language_89801.svg" />
-                    <img alt="idiomaIngles" id="english" src="../images/icons8-gran-bretana-48.png" />
-                </label>
-
-                <div id="bolsaCompra">
-                    <a href="#"><img id='icono_bolsaCompra' alt="bolsaCompra"
-                        src="../images/shopping-bag_icon-icons.com_69305.svg" /></a>
-                    <div>
-                        <p></p>
-                    </div>
+        <div>
+            <header>
+                <div id="menu">
+                    <input id="hamburguer-icon" type="checkbox" />
+                    <label for="hamburguer-icon">
+                        <span className="hamburguer-line"></span>
+                        <span className="hamburguer-line"></span>
+                        <span className="hamburguer-line"></span>
+                    </label>
+                    <ul id="menuOculto">
+                        <li>
+                            <div className='buscador'>
+                                {/* <img alt="lupa" src="../images/search_magnifying_glass_icon_149392.svg" /> */}
+                                <input type="text" id="buscar" name="buscar" placeholder="Encuentra lo que buscas" />
+                                <button id='btnBuscar'>Buscar</button>
+                            </div>
+                            <hr />
+                        </li>
+                        <li> <a href="#">Productos</a></li>
+                        <li> <a href="#">ALOJÚ</a></li>
+                        <li> <a href="#">Contáctanos</a></li>
+                        <li> <a href="#">Ayuda</a></li>
+                    </ul>
                 </div>
-                <div id='menu_oculto_bolsa'>
-                    <div id='cerrar_menu_bolsa'>
-                        <picture>
-                            <img id='icono_cerrar_bolsa' alt='cerrar' src='../images/cross-symbol_icon-icons.com_74149.png' />
-                        </picture>
-                        <p id='cerrar_bolsa'>CERRAR</p>
+
+                <div id="logoCabecera">
+                    <Link to='/home'>
+                        <img alt="logoColor" src="../images/logotipo_en_color.png" />
+                    </Link>
+                </div>
+
+                <div id='menu_query'>
+                    <p id='productos_menu_query'><a href="#">Productos</a></p>
+                    <p id='personalizacion_menu_query'><a href="#">ALOJÚ</a></p>
+                    <p id='aloju_menu_query'><a href="#">Contáctanos</a></p>
+                    <p id='contactanos_menu_query'><a href="#">Ayuda</a></p>
+                </div>
+
+                <div id="derecha">
+                    <Link to='/login'>
+                        <img id='icon_user' alt="icono_user" src="../images/icon_user.svg" />
+                    </Link>
+                    <input id="idioma" type="checkbox" />
+                    <label for="idioma">
+                        <img alt="globoIdioma" src="../images/-language_89801.svg" />
+                        <img alt="idiomaIngles" id="english" src="../images/icons8-gran-bretana-48.png" />
+                    </label>
+
+                    <div id="bolsaCompra">
+                        <a href="#"><img id='icono_bolsaCompra' alt="bolsaCompra"
+                            src="../images/shopping-bag_icon-icons.com_69305.svg" /></a>
+                        <div>
+                            <p></p>
+                        </div>
                     </div>
-                    <div id='contenido_menu_compra'>
-                        <p>CARRITO DE COMPRA</p>
-                        <hr />
-                        <div id='contenido_compra'>
+                    <div id='menu_oculto_bolsa'>
+                        <div id='cerrar_menu_bolsa'>
                             <picture>
+                                <img id='icono_cerrar_bolsa' alt='cerrar' src='../images/cross-symbol_icon-icons.com_74149.png' />
+                            </picture>
+                            <p id='cerrar_bolsa'>CERRAR</p>
+                        </div>
+                        <div id='contenido_menu_compra'>
+                            <p>CARRITO DE COMPRA</p>
+                            <hr />
+                            <div id='contenido_compra'>
+                                {/* <picture>
                                 <img src="images/nodisponible.png" alt="Imagen No Disponible" />
                             </picture>
                             <p id='nombre_producto'>Nombre producto</p>
@@ -74,15 +78,21 @@ const Header = (props) => {
                             <picture>
                                 <img id='eliminar_producto' alt='eliminar_producto'
                                     src='../images/cross-symbol_icon-icons.com_74149.png' />
-                            </picture>
+                            </picture> */}
+                            </div>
+                            <hr />
+                            <button id='comprar_desde_menu'>COMPRAR</button>
+                            <button id='ver_carrito'>VER CARRITO</button>
                         </div>
-                        <hr />
-                        <button id='comprar_desde_menu'>COMPRAR</button>
-                        <button id='ver_carrito'>VER CARRITO</button>
                     </div>
                 </div>
+            </header>
+            <div className='buscadorQuery'>
+                {/* <img alt="lupa" src="../images/search_magnifying_glass_icon_149392.svg" /> */}
+                <input type="text" id="buscar" name="buscar" placeholder="Encuentra lo que buscas" />
+                <button id='btnBuscar'>Buscar</button>
             </div>
-        </header>
+        </div>
     )
 }
 
--- src/jq.js
+++ src/jq.js
@@ -2,4 +2,34 @@ import $ from 'jquery'
 
 $(document).ready(function () {
 
-})
+    /*Abre y oculta el menu de la cesta de compra.
+    Si esta abierto el menu normal, se cierra*/
+    $('#bolsaCompra').click(function () {
+        $('#menu_oculto_bolsa').css('right', '0')
+        $('header div#menu input').prop('checked', false)
+    })
+
+    $('#cerrar_bolsa').click(function () {
+        $('#menu_oculto_bolsa').css('right', '-100%')
+    })
+
+    $('#icono_cerrar_bolsa').click(function () {
+        $('#menu_oculto_bolsa').css('right', '-100%')
+    })
+
+    $('#menu').click(function () {
+        $('#menu_oculto_bolsa').css('right', '-100%')
+    })
+
+    //Actualiza el número de caracteres restantes 
+    var max_caracteres = 100;
+
+    $('#restantes').html(max_caracteres);
+
+    $('textarea#comentario').keypress(function () {
+        var caracter = $(this).val().length;
+        var diferencia = max_caracteres - caracter;
+        $('#restantes').html(diferencia);
+    });
+
+});
