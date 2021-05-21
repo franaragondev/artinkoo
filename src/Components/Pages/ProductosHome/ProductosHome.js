7e9e85201f16e3885dcb90d7612b45a7aae664a6
franaragon13 <franaragon196913@gmail.com>
2021-05-21 16:26:43 +0200

    Añade productos más vendidos a página home

--- src/Components/Pages/ProductosHome/ProductosHome.js
+++ src/Components/Pages/ProductosHome/ProductosHome.js
@@ -1,11 +1,39 @@
 import React, { Component, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Axios from 'axios'
 
 //Componente que renderizará los productos situados en el HOME
 const ProductosHome = (props) => {
+    const [productosMasVendidos, setProductosMasVendidos] = useState([]);
+
+    useEffect(() => {
+        Axios.get(`https://artinkoo.herokuapp.com/masVendidos`).then((response) => {
+            setProductosMasVendidos(response.data)
+            console.log(response);
+        })
+    }, [])
+
     return (
         <div>
-
+            <section id="masVendidos">
+                <div id="tituloVendidos">
+                    <hr />
+                    <h2 className='tituloPagina'>LO MÁS VENDIDO</h2>
+                    <hr />
+                </div>
+                {productosMasVendidos.map((producto, index) => {
+                    return (
+                        <article>
+                            <a href="#">
+                                <picture><img src={'../images/productos/' + producto.idProducto + '.jpg'} alt={'Imagen producto ' + producto.idProducto} /></picture>
+                            </a>
+                            <h4 className='nombreProducto'>{producto.nombre} | {producto.precio}€</h4>
+                            <a href="#"><span>AÑADIR A LA CESTA</span><i class="fas fa-shopping-cart"></i></a>
+                        </article>
+                    )
+                })}
+                <button id="mostrarMasVendidos">MOSTRAR MÁS</button>
+            </section>
         </div>
     )
 }
