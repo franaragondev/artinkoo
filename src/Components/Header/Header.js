bbe8c3591ec4c4a758c5f0433cbccbc9fcb72c0f
franaragon13 <franaragon196913@gmail.com>
2021-05-24 12:31:04 +0200

    Añade link para descargar app

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = (props) => {
                         <li> <Link to='/productos'>Productos</Link></li>
                         <li> <a href="#">ALOJÚ</a></li>
                         <li> <Link to='/contactanos'>Contáctanos</Link></li>
-                        <li> <a href="#">Descarga la App</a></li>
+                        <li> <a href="https://storage.googleapis.com/appilder/app/da4a6e7e021b2ad856b37a8607fd158b/fc9a517a0c1ac15c718f8f33c968509a.apk?GoogleAccessId=desktopapi%40appilder-com.iam.gserviceaccount.com&Expires=1622715878&Signature=oIwwNLml7c7q7mMxB1jvezpotjnsumfFgNZ7sQukmyEWZTvn%2BSFCgT9GEKwQJzUfLCwfwlvPDADM6hvvw13gpYbEReuDxoA4pvulE%2FcFotCfO1zhRTReEGqqV58GbzVvtKFxVRhIPI5YC1lqfwT%2FjtHbJ3oi4n7%2BtPJ6c%2Bt5vUYbvjtVwTjcPdbrPbW1Sq8YHQMPZJlsQ5%2BzlXw6DTaEqppHmfMGg5iIynE3osQkqoF6YZxp%2FREhZLNs3EZ1%2BnFCPHZNiwlVRv7kQTy%2FtZVFvNbddQLDDI26AV8ROPszYYAbbbIZim2E7PDP0JSUSCuu1MgAsP3URMqHKX4lWisx%2BA%3D%3D">Descarga la App</a></li>
                     </ul>
                 </div>
 
