cc7e471848b52d21216aa94bae83e27e3ed6125e
franaragon13 <franaragon196913@gmail.com>
2021-05-25 13:59:43 +0200

    Añade estilos a página de quienes somos

--- src/Components/Pages/QuienesSomos/QuienesSomos.js
+++ src/Components/Pages/QuienesSomos/QuienesSomos.js
@@ -17,7 +17,7 @@ const QuienesSomos = (props) => {
             <div className='paginasFooter'>
                 <img className='imagenLegales' alt='quienes_somos' src='../images/pexels-sharon-mccutcheon-1148998.jpg'></img>
             </div>
-            <p className='parrafo_paginas_legales'>Desde artinkoo buscamos transmitir a nuestros clientes felicidad y color en su día a día a través de nuestra web de personalización de una gran variedad de productos y es por eso mismo, por lo que contamos con un diseño web colorido a la par que profesional.<br />En artinkoo contamos con la filosofía de “por qué vivir en un mundo igual que los demás cuando puedes crearlo tú mismo”  y es por ello por lo que queremos que cada cliente que visite nuestra web se vaya con la satisfacción y el pensamiento de que quiere vivir en un mundo a su medida. </p>
+            <p className='parrafo_paginas_legales'>Desde artinkoo buscamos transmitir a nuestros clientes felicidad y color en su día a día a través de nuestra web de personalización de una gran variedad de productos y es por eso mismo, por lo que contamos con un diseño web colorido a la par que profesional.<br /><br />En artinkoo contamos con la filosofía de “por qué vivir en un mundo igual que los demás cuando puedes crearlo tú mismo”  y es por ello por lo que queremos que cada cliente que visite nuestra web se vaya con la satisfacción y el pensamiento de que quiere vivir en un mundo a su medida. </p>
             <Footer />
         </div>
     )
