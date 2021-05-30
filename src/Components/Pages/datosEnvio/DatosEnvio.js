5848905d7559fbb69cb830f06290b3ecbdcc4753
franaragon13 <franaragon196913@gmail.com>
2021-05-30 13:14:21 +0200

    Añade funcionalidad para editar la dirección de envío  a la hora de comprar

--- src/Components/Pages/datosEnvio/DatosEnvio.js
+++ src/Components/Pages/datosEnvio/DatosEnvio.js
@@ -11,6 +11,11 @@ import Header from '../../Header/HeaderCarrito'
 const DatosEnvio = (props) => {
     const [cambiarContacto, setCambiarContanto] = useState(false)
     const [editarEmail, setEditarEmail] = useState('')
+    const [cambiarDireccion, setCambiarDireccion] = useState(false)
+    const [editarDireccion, setEditarDireccion] = useState('')
+    const [editarCiudad, setEditarCiudad] = useState('')
+    const [editarProvincia, setEditarProvincia] = useState('')
+    const [editarCodigoPostal, setEditarCodigoPostal] = useState('')
     const cookies = new Cookies()
 
     const cambiarTituloyContacto = () => {
@@ -19,6 +24,15 @@ const DatosEnvio = (props) => {
             document.getElementById('cambiar_datos').innerHTML = 'Cancelar'
         } else {
             document.getElementById('cambiar_datos').innerHTML = 'Cambiar'
+        }
+    }
+
+    const cambiarTituloyDireccion = () => {
+        setCambiarDireccion(!cambiarDireccion)
+        if (!cambiarDireccion) {
+            document.getElementById('cambiar_datos1').innerHTML = 'Cancelar'
+        } else {
+            document.getElementById('cambiar_datos1').innerHTML = 'Cambiar'
         }
     }
 
@@ -64,6 +78,51 @@ const DatosEnvio = (props) => {
         }
     }
 
+    const actualizarDireccion = async () => {
+        if (editarDireccion != '' && editarCiudad != '' && editarProvincia != '' && editarCodigoPostal != '') {
+            await Axios.post('http://localhost:8000/actualizarDireccion',
+                // await Axios.post('https://artinkoo.herokuapp.com/actualizarEmail',
+                { direccion: editarDireccion, ciudad: editarCiudad, provincia: editarProvincia, codigoPostal: editarCodigoPostal, idUsuario: cookies.get('idUsuario') })
+                .then(response => {
+                    if (response.statusText == 'OK') {
+                        cookies.set('direccion', editarDireccion, { path: '/' })
+                        cookies.set('ciudad', editarCiudad, { path: '/' })
+                        cookies.set('provincia', editarProvincia, { path: '/' })
+                        cookies.set('codigoPostal', editarCodigoPostal, { path: '/' })
+                        swal({
+                            title: "Dirección Actualizada",
+                            text: `Su dirección de envío ha sido actualizada correctamente.`,
+                            icon: "success",
+                            button: "Ok!",
+                        }).then(function () {
+                            window.location.href = 'http://localhost:3000/datosEnvio'
+                            // window.location.href = 'https://proyecto-final-fran-aragon.netlify.app/datosEnvio'
+                        })
+                    } else {
+                        swal({
+                            title: "Oh! Algo ha fallado.",
+                            text: "Inténtelo de nuevo más tarde.",
+                            icon: "error",
+                            button: "Volver",
+                        }).then(function () {
+                            window.location.href = 'http://localhost:3000/datosEnvio'
+                            // window.location.href = 'https://proyecto-final-fran-aragon.netlify.app/datosEnvio'
+                        })
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        } else {
+            swal({
+                title: "Oh! Tiene campos vacíos.",
+                text: "Por favor, rellene todos los campos para poder actualizar la direccióñn de envío.",
+                icon: "error",
+                button: "Volver",
+            })
+        }
+    }
+
     if (cookies.get('nombre')) {
         return (
             <div>
@@ -90,8 +149,19 @@ const DatosEnvio = (props) => {
                         </div>
                         <div id='datos_contacto'>
                             <p>Dirección</p>
-                            <p id='cambiar_datos'>Cambiar</p>
-                            <p>{cookies.get('direccion') + ', ' + cookies.get('ciudad') + ', ' + cookies.get('provincia') + ', ' + cookies.get('codigoPostal')}</p>
+                            <p id='cambiar_datos1' onClick={() => cambiarTituloyDireccion()}>Cambiar</p>
+                            {cambiarDireccion
+                                ?
+                                <div className='editarDireccionEnvio'>
+                                    <input className='cambiarContacto' placeholder='Introduzca dirección' onChange={(e) => { setEditarDireccion(e.target.value) }}></input>
+                                    <input className='cambiarContacto' placeholder='Introduzca ciudad' onChange={(e) => { setEditarCiudad(e.target.value) }}></input>
+                                    <input className='cambiarContacto' placeholder='Introduzca provincia' onChange={(e) => { setEditarProvincia(e.target.value) }}></input>
+                                    <input className='cambiarContacto' placeholder='Introduzca código postal' onChange={(e) => { setEditarCodigoPostal(e.target.value) }}></input>
+                                    <button onClick={() => actualizarDireccion()} className='btnCambiarDireccion'>Actualizar</button>
+                                </div>
+                                :
+                                <p>{cookies.get('direccion') + ', ' + cookies.get('ciudad') + ', ' + cookies.get('provincia') + ', ' + cookies.get('codigoPostal')}</p>
+                            }
                         </div>
                     </div>
 
