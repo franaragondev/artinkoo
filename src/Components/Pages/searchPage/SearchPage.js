4542ec8a2bcb234121fa3c4f204ee495ee6e542b
franaragon13 <franaragon196913@gmail.com>
2021-05-31 12:05:00 +0200

    Añade funcionalidad para poder añadir a cesta productos buscados en el buscador

--- src/Components/Pages/searchPage/SearchPage.js
+++ src/Components/Pages/searchPage/SearchPage.js
@@ -5,12 +5,15 @@ import { useParams } from "react-router-dom";
 import Footer from '../../Footer/Footer'
 import GoToTop from '../../GoToTop/GoToTop'
 import Header from '../../Header/Header'
+import Cookies from 'universal-cookie'
+import swal from 'sweetalert';
 
 //Componente que renderizará la página PRODUCTOS
 const SearchPage = (props) => {
     const { aBuscar } = useParams()
     const [productos, setProductos] = useState([]);
     const [buscada, setBuscada] = useState(aBuscar)
+    const cookies = new Cookies()
 
     if (buscada != aBuscar) {
         Axios.get(`https://artinkoo.herokuapp.com/buscar/${aBuscar}`).then((response) => {
@@ -28,6 +31,35 @@ const SearchPage = (props) => {
             console.log(response.data);
         })
     }, [])
+
+    const añadirCesta = (idProducto) => {
+        if (cookies.get('idUsuario')) {
+            Axios.post(`https://artinkoo.herokuapp.com/anadirCesta`, { idUsuario: cookies.get('idUsuario'), idProducto: idProducto }).then((response) => {
+                // Axios.post(`http://localhost:8000/anadirCesta`, { idUsuario: cookies.get('idUsuario'), idProducto: idProducto }).then((response) => {
+                if (response.data.affectedRows == 1) {
+                    swal({
+                        title: "¡Tienes un producto nuevo esperándote!",
+                        text: 'Producto añadido a la cesta correctamente.',
+                        icon: "success",
+                        button: "Ok!",
+                    }).then(function () {
+                        // window.location.href = 'http://localhost:3000/home'
+                        window.location.href = 'https://proyecto-final-fran-aragon.netlify.app/home'
+                    })
+                }
+            })
+        } else {
+            swal({
+                title: "Oh! Parece que no estás logueado.",
+                text: 'Por favor, loguéate o registrate antes de añadir productos a tu cesta.',
+                icon: "error",
+                button: "Ok!",
+            }).then(function () {
+                // window.location.href = 'http://localhost:3000/login'
+                window.location.href = 'https://proyecto-final-fran-aragon.netlify.app/login'
+            })
+        }
+    }
 
 
     return (
@@ -48,7 +80,7 @@ const SearchPage = (props) => {
                                     <picture><img src={'../images/productos/' + producto.idProducto + '.jpg'} alt={'Imagen producto ' + producto.idProducto} /></picture>
                                 </Link>
                                 <h4 className='nombreProducto'>{producto.nombre} | {producto.precio}€</h4>
-                                <a href="#"><span>AÑADIR A LA CESTA</span></a>
+                                <span onClick={() => añadirCesta(producto.idProducto)}>AÑADIR A LA CESTA</span>
                             </article>
 
                         )
